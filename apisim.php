a44aef1cdc0597038a733f11a4c3e319cfbece20
Siggi Apacheweb <siggib007@users.noreply.github.com>
2023-11-25 17:49:39 +0000

    Dynamic Update

--- apisim.php
+++ apisim.php
@@ -1,122 +1,5 @@
 <?php
-  function HTTPCodeLookup ($iHTTPCode)
-  {
-    // Response texts from https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
-    // and http://php.net/manual/en/function.http-response-code.php
-    switch ($iHTTPCode)
-    {
-      case 200: $text = 'OK'; break;
-      case 201: $text = 'Created'; break;
-      case 202: $text = 'Accepted'; break;
-      case 203: $text = 'Non-Authoritative Information'; break;
-      case 204: $text = 'No Content'; break;
-      case 205: $text = 'Reset Content'; break;
-      case 206: $text = 'Partial Content'; break;
-      case 207: $text = 'Multi-Status'; break;
-      case 208: $text = 'Already Reported'; break;
-      case 226: $text = 'IM Used'; break;
-      case 300: $text = 'Multiple Choices'; break;
-      case 301: $text = 'Moved Permanently'; break;
-      case 302: $text = 'Found'; break;
-      case 303: $text = 'See Other'; break;
-      case 304: $text = 'Not Modified'; break;
-      case 305: $text = 'Use Proxy'; break;
-      case 306: $text = 'Switch Proxy'; break;
-      case 307: $text = 'Temporarily Redirect'; break;
-      case 308: $text = 'Permanently Redirect'; break;
-      case 400: $text = 'Bad Request'; break;
-      case 401: $text = 'Unauthorized'; break;
-      case 402: $text = 'Payment Required'; break;
-      case 403: $text = 'Forbidden'; break;
-      case 404: $text = 'Not Found'; break;
-      case 405: $text = 'Method Not Allowed'; break;
-      case 406: $text = 'Not Acceptable'; break;
-      case 407: $text = 'Proxy Authentication Required'; break;
-      case 408: $text = 'Request Time-out'; break;
-      case 409: $text = 'Conflict'; break;
-      case 410: $text = 'Gone'; break;
-      case 411: $text = 'Length Required'; break;
-      case 412: $text = 'Precondition Failed'; break;
-      case 413: $text = 'Request Entity Too Large'; break;
-      case 414: $text = 'Request-URI Too Large'; break;
-      case 415: $text = 'Unsupported Media Type'; break;
-      case 416: $text = 'Range Not Satisfiable'; break;
-      case 417: $text = 'Expectation Failed'; break;
-      case 418: $text = 'I am a teapot'; break;
-      case 421: $text = 'Misdirected Request'; break;
-      case 422: $text = 'Unprocessable Entity'; break;
-      case 423: $text = 'Locked'; break;
-      case 424: $text = 'Failed Dependency'; break;
-      case 426: $text = 'Upgrade Required'; break;
-      case 428: $text = 'Precondition Required'; break;
-      case 429: $text = 'Too Many Requests'; break;
-      case 431: $text = 'Request Header Fields Too Large'; break;
-      case 451: $text = 'Unavailable For Legal Reasons '; break;
-      case 500: $text = 'Internal Server Error'; break;
-      case 501: $text = 'Not Implemented'; break;
-      case 502: $text = 'Bad Gateway'; break;
-      case 503: $text = 'Service Unavailable'; break;
-      case 504: $text = 'Gateway Time-out'; break;
-      case 505: $text = 'HTTP Version not supported'; break;
-      case 506: $text = 'Variant Also Negotiates'; break;
-      case 507: $text = 'Insufficient Storage'; break;
-      case 508: $text = 'Loop Detected'; break;
-      case 510: $text = 'Not Extended'; break;
-      case 511: $text = 'Network Authentication Required'; break;
-      //Unoffical Codes, No RFC backing but common use
-      case 0:   $text = 'No Code';
-      case 103: $text = 'Checkpoint'; break;
-      case 419: $text = 'I am a fox'; break;
-      case 420: $text = 'Enhance Your Calm'; break;
-      case 450: $text = 'Blocked by Parental Controls'; break;
-      case 498: $text = 'Invalid Token'; break;
-      case 499: $text = 'Request has been forbidden by antivirus '; break;
-      case 509: $text = 'Bandwidth Limit Exceeded'; break;
-      case 530: $text = 'Site is frozen'; break;
-      case 598: $text = 'Network read timeout error'; break;
-      case 599: $text = 'Network connect timeout error'; break;
-      case 440: $text = 'Login Time-out'; break;
-      case 449: $text = 'Retry with ...'; break;
-      case 551: $text = 'Exchange Activesync Redirect'; break;
-      case 444: $text = 'No Response'; break;
-      case 495: $text = 'SSL Cert Error'; break;
-      case 496: $text = 'SSL Cert Required'; break;
-      case 497: $text = 'HTTP Request sent to HTTPS port'; break;
-      case 499: $text = 'Client Closed Request'; break;
-      case 520: $text = 'Unknown Error'; break;
-      case 521: $text = 'Web Server Is Down'; break;
-      case 522: $text = 'Connection Timed Out'; break;
-      case 523: $text = 'Origin Is Unreachable'; break;
-      case 524: $text = 'A Timeout Occurred'; break;
-      case 525: $text = 'SSL Handshake Failed'; break;
-      case 526: $text = 'Invalid SSL Certificate'; break;
-      case 527: $text = 'Railgun Error'; break;
-      case 666: $text = 'Devil is ready'; break;
-      default:  $text = 'Random Status Code'; break;
-    }
-    return $text;
-  }
-
-  function Array2XML ($ComplexArray)
-  {
-    $strXML = "";
-    foreach ($ComplexArray as $key => $value)
-    {
-      if (is_array($value))
-      {
-        $strXML .= "<$key>\n";
-        $strXML .= Array2XML($value);
-        $strXML .= "</$key>\n";
-      }
-      else
-      {
-        $value = str_replace("&", "&amp;", $value);
-        $strXML .= "<$key>$value</$key>\n";
-      }
-    }
-    return $strXML;
-  }
-
+  require("DBCon.php");
   $strURL = $_SERVER["SERVER_NAME"];
   $strURI = $_SERVER["REQUEST_URI"];
   $Protocol = $_SERVER['SERVER_PROTOCOL'];
@@ -202,26 +85,64 @@
 
 
   $iArgCount = count($_GET);
-  $strTitle = "API Simulator";
+  $strTitle = $ConfArray["ProductName"];
+  $RCLower = $ConfArray["rclower"];
+  $RCUpper = $ConfArray["rcupper"];
+  $SampleNum = $ConfArray["samplenum"];
+  $SampleResult = $ConfArray["SampleResult"];
   $strTestResp = "You asked for code $iResponse $text";
   $strSleepResp = "As requested I took a $iSleep second nap";
   $strTestError = "You asked for Response Code $iResponse. " .
-    " Response codes less than 200 or greater than 999 are not supported";
-  $strIntro = "This page will allow you test your code for abnormal responses from API calls, " .
-    "such as wrong format, slow response, or response codes (aka error codes) " .
-    "such as HTTP 418 :-D\n" .
-    "Please contact siggi@supergeek.us with any questions, comments and complients.\n" .
-  $OptionsArray["Options"]["rc"]["command"] = "$PageURL?rc=234";
-  $OptionsArray["Options"]["rc"]["descr"] = "Sets the HTTP response code to 234, ".
-    "any number between 200 and 999 supported";
-  $OptionsArray["Options"]["sleep"]["command"] = "$PageURL?sleep=123";
-  $OptionsArray["Options"]["sleep"]["descr"] = "Sleeps for 123 seconds before responding";
-  $OptionsArray["Options"]["format"]["command"] = "$PageURL?result=html";
-  $OptionsArray["Options"]["format"]["descr"] = "Overwrites the accepted header and specifies that the reponse should be in HTML. " .
-    "Valid formats (case insensitive): HTML, XML, JSON, TXT and NONE. " .
-    "Default format is HTML" ;
-  $OptionsArray["OptionsDescr"] ="These can of course be chained together such as " .
-    " $PageURL?sleep=234&rc=234";
+    " Response codes less than $RCLower or greater than $RCUpper are not supported";
+  $strIntro = $TextArray["intro"];
+  $strExample = "$PageURL?";
+
+  $strQuery = "SELECT vcOptionCode, vcOptionDescr, vcValueType FROM tblOptions";
+  $QueryData = QuerySQL($strQuery);
+  if($QueryData[0] > 0)
+  {
+    foreach($QueryData[1] as $Row)
+    {
+      $OptionCode = $Row["vcOptionCode"];
+      $OptionDescr = $Row["vcOptionDescr"];
+      $OptionType = $Row["vcValueType"];
+      switch ($OptionType)
+      {
+        case "int":
+          $SampleValue = $SampleNum;
+          break;
+        case "string":
+          $SampleValue = $SampleResult;
+          break;
+        default:
+          $SampleValue = "";
+          break;
+      }
+      $OptionDescr = str_replace("xyz", "$SampleNum", $OptionDescr);
+      $OptionDescr = str_replace("xx", "$RCLower", $OptionDescr);
+      $OptionDescr = str_replace("yy", "$RCUpper", $OptionDescr);
+      $OptionDescr = str_replace("xystr", "$SampleResult", $OptionDescr);
+      $OptionsArray["Options"][$OptionCode]["command"] = "$PageURL?$OptionCode=$SampleValue";
+      $OptionsArray["Options"][$OptionCode]["descr"] = "Requires $OptionType. $OptionDescr";
+      $strExample .= "$OptionCode=$SampleValue&";
+    }
+  }
+  else
+  {
+    if($QueryData[0] == 0)
+    {
+      error_log("query of $strQuery returned no rows");
+    }
+    else
+    {
+      $strMsg = Array2String($QueryData[1]);
+      error_log("Query of $strQuery did not return data. Rowcount: $QueryData[0] Msg:$strMsg");
+      printPg($ErrMsg,"error");
+    }
+  }
+
+  $strExample = rtrim($strExample,"&");
+  $OptionsArray["OptionsDescr"] = $TextArray["OptDescr"] . $strExample;
 
   $ReturnArray[$PagebaseName]["ReceivedArgsCount"] = $iArgCount;
   $ReturnArray[$PagebaseName]["ReceivedArgs"] = $_GET;
@@ -229,7 +150,7 @@
   if ($iResponse!=0)
   {
     $ReturnArray[$PagebaseName]["ProcessResp"]["HTTPCode"] = $iResponse;
-    if ($iResponse < 200 or $iResponse>999)
+    if ($iResponse < $RCLower or $iResponse > $RCUpper)
     {
       $ReturnArray[$PagebaseName]["ProcessResp"]["TestResp"] = $strTestError;
     }
@@ -253,7 +174,6 @@
     case "html":
       require("header.php");
 
-      print "<p class=\"MainText\">\nAccept: $AcceptHeader</p>\n";
       if ($iArgCount>0)
       {
         print "<h3>Received $iArgCount parameters:</h3>";
--- functions.php
+++ functions.php
@@ -81,6 +81,125 @@
     return json_encode($input);
   }
 
+  function Array2XML ($ComplexArray)
+  {
+    $strXML = "";
+    foreach ($ComplexArray as $key => $value)
+    {
+      if (is_array($value))
+      {
+        $strXML .= "<$key>\n";
+        $strXML .= Array2XML($value);
+        $strXML .= "</$key>\n";
+      }
+      else
+      {
+        $value = str_replace("&", "&amp;", $value);
+        $strXML .= "<$key>$value</$key>\n";
+      }
+    }
+    return $strXML;
+  }
+
+  function HTTPCodeLookup ($iHTTPCode)
+  {
+    // Response texts from https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+    // and http://php.net/manual/en/function.http-response-code.php
+    switch ($iHTTPCode)
+    {
+      case 200: $text = 'OK'; break;
+      case 201: $text = 'Created'; break;
+      case 202: $text = 'Accepted'; break;
+      case 203: $text = 'Non-Authoritative Information'; break;
+      case 204: $text = 'No Content'; break;
+      case 205: $text = 'Reset Content'; break;
+      case 206: $text = 'Partial Content'; break;
+      case 207: $text = 'Multi-Status'; break;
+      case 208: $text = 'Already Reported'; break;
+      case 226: $text = 'IM Used'; break;
+      case 300: $text = 'Multiple Choices'; break;
+      case 301: $text = 'Moved Permanently'; break;
+      case 302: $text = 'Found'; break;
+      case 303: $text = 'See Other'; break;
+      case 304: $text = 'Not Modified'; break;
+      case 305: $text = 'Use Proxy'; break;
+      case 306: $text = 'Switch Proxy'; break;
+      case 307: $text = 'Temporarily Redirect'; break;
+      case 308: $text = 'Permanently Redirect'; break;
+      case 400: $text = 'Bad Request'; break;
+      case 401: $text = 'Unauthorized'; break;
+      case 402: $text = 'Payment Required'; break;
+      case 403: $text = 'Forbidden'; break;
+      case 404: $text = 'Not Found'; break;
+      case 405: $text = 'Method Not Allowed'; break;
+      case 406: $text = 'Not Acceptable'; break;
+      case 407: $text = 'Proxy Authentication Required'; break;
+      case 408: $text = 'Request Time-out'; break;
+      case 409: $text = 'Conflict'; break;
+      case 410: $text = 'Gone'; break;
+      case 411: $text = 'Length Required'; break;
+      case 412: $text = 'Precondition Failed'; break;
+      case 413: $text = 'Request Entity Too Large'; break;
+      case 414: $text = 'Request-URI Too Large'; break;
+      case 415: $text = 'Unsupported Media Type'; break;
+      case 416: $text = 'Range Not Satisfiable'; break;
+      case 417: $text = 'Expectation Failed'; break;
+      case 418: $text = 'I am a teapot'; break;
+      case 421: $text = 'Misdirected Request'; break;
+      case 422: $text = 'Unprocessable Entity'; break;
+      case 423: $text = 'Locked'; break;
+      case 424: $text = 'Failed Dependency'; break;
+      case 426: $text = 'Upgrade Required'; break;
+      case 428: $text = 'Precondition Required'; break;
+      case 429: $text = 'Too Many Requests'; break;
+      case 431: $text = 'Request Header Fields Too Large'; break;
+      case 451: $text = 'Unavailable For Legal Reasons '; break;
+      case 500: $text = 'Internal Server Error'; break;
+      case 501: $text = 'Not Implemented'; break;
+      case 502: $text = 'Bad Gateway'; break;
+      case 503: $text = 'Service Unavailable'; break;
+      case 504: $text = 'Gateway Time-out'; break;
+      case 505: $text = 'HTTP Version not supported'; break;
+      case 506: $text = 'Variant Also Negotiates'; break;
+      case 507: $text = 'Insufficient Storage'; break;
+      case 508: $text = 'Loop Detected'; break;
+      case 510: $text = 'Not Extended'; break;
+      case 511: $text = 'Network Authentication Required'; break;
+      //Unoffical Codes, No RFC backing but common use
+      case 0:   $text = 'No Code';
+      case 103: $text = 'Checkpoint'; break;
+      case 419: $text = 'I am a fox'; break;
+      case 420: $text = 'Enhance Your Calm'; break;
+      case 450: $text = 'Blocked by Parental Controls'; break;
+      case 498: $text = 'Invalid Token'; break;
+      case 499: $text = 'Request has been forbidden by antivirus '; break;
+      case 509: $text = 'Bandwidth Limit Exceeded'; break;
+      case 530: $text = 'Site is frozen'; break;
+      case 598: $text = 'Network read timeout error'; break;
+      case 599: $text = 'Network connect timeout error'; break;
+      case 440: $text = 'Login Time-out'; break;
+      case 449: $text = 'Retry with ...'; break;
+      case 551: $text = 'Exchange Activesync Redirect'; break;
+      case 444: $text = 'No Response'; break;
+      case 495: $text = 'SSL Cert Error'; break;
+      case 496: $text = 'SSL Cert Required'; break;
+      case 497: $text = 'HTTP Request sent to HTTPS port'; break;
+      case 499: $text = 'Client Closed Request'; break;
+      case 520: $text = 'Unknown Error'; break;
+      case 521: $text = 'Web Server Is Down'; break;
+      case 522: $text = 'Connection Timed Out'; break;
+      case 523: $text = 'Origin Is Unreachable'; break;
+      case 524: $text = 'A Timeout Occurred'; break;
+      case 525: $text = 'SSL Handshake Failed'; break;
+      case 526: $text = 'Invalid SSL Certificate'; break;
+      case 527: $text = 'Railgun Error'; break;
+      case 666: $text = 'Devil is ready'; break;
+      default:  $text = 'Random Status Code'; break;
+    }
+    return $text;
+  }
+
+
   function QuerySQL($strQuery)
   {
     $dbh = $GLOBALS["dbh"];
